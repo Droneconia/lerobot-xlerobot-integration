f6f97e9cd96fc86e2a08474ed85ff66c10e376fb
alexkoven <40674821+alexkoven@users.noreply.github.com>
2025-11-28 20:22:06 -0600

    remap fix

--- train_grievous.sh
+++ train_grievous.sh
@@ -64,9 +64,7 @@ verify_setup() {
         --eval_freq=0 \
         --wandb.enable=false \
         --policy.push_to_hub=false \
-        --rename_map.observation.images.left_wrist=observation.images.camera1 \
-        --rename_map.observation.images.right_wrist=observation.images.camera2 \
-        --rename_map.observation.images.head=observation.images.camera3 \
+        --rename_map='{"observation.images.left_wrist":"observation.images.camera1","observation.images.right_wrist":"observation.images.camera2","observation.images.head":"observation.images.camera3"}' \
         --seed=1000
     
     echo "✓ Verification complete!"
@@ -96,9 +94,7 @@ test_training() {
         --wandb.enable=${WANDB_ENABLE} \
         --wandb.project=${WANDB_PROJECT} \
         --policy.push_to_hub=false \
-        --rename_map.observation.images.left_wrist=observation.images.camera1 \
-        --rename_map.observation.images.right_wrist=observation.images.camera2 \
-        --rename_map.observation.images.head=observation.images.camera3 \
+        --rename_map='{"observation.images.left_wrist":"observation.images.camera1","observation.images.right_wrist":"observation.images.camera2","observation.images.head":"observation.images.camera3"}' \
         --seed=1000
     
     echo "✓ Test training complete!"
@@ -134,9 +130,7 @@ full_training() {
         --wandb.project=${WANDB_PROJECT} \
         --policy.repo_id=${POLICY_REPO_ID} \
         --policy.push_to_hub=${PUSH_TO_HUB} \
-        --rename_map.observation.images.left_wrist=observation.images.camera1 \
-        --rename_map.observation.images.right_wrist=observation.images.camera2 \
-        --rename_map.observation.images.head=observation.images.camera3 \
+        --rename_map='{"observation.images.left_wrist":"observation.images.camera1","observation.images.right_wrist":"observation.images.camera2","observation.images.head":"observation.images.camera3"}' \
         --seed=1000
     
     echo "✓ Full training complete!"
